feb703ed04423813cc207041a7ce6a41c30f92f4
yongsyongs <ysn3830@gmail.com>
2020-09-28 20:28:58 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from common.dataset import H36MDataset
+from common.signal_processing import ExponentialMovingAverage
 
 dataset = H36MDataset()
-dataset.get_generator('cpn', padding=True, length=1, chunked=True, receptive_field=27)
+dataset.get_generator('cpn', padding=True, length=1, chunked=True, receptive_field=243, preprocessor=ExponentialMovingAverage(0.9))
+
+# lr reset by epoch w/ read() func
