eeff618634d24db7128efd4df24e2f0157dd0870
yongsyongs <ysn3830@gmail.com>
2020-09-29 06:53:10 +0900

    code to train by config

--- common/Experiments/train.py
+++ common/Experiments/train.py
@@ -1,19 +1,9 @@
 import torch
+from torchsummary import summary
 from common.loss import mpjpe
 
 
-def train(pipeline=None, epochs=100, dataset=None, config=None):
-    train_generator = dataset.get_generator(train=True, **config.dataset_args)
-    test_generator = dataset.get_generator(train=False, **config.dataset_args)
-
-    for epoch in range(epochs):
-
-
-
-def _train_normal(pipeline, train_gen, test_gen):
-    with torch.set_grad_enabled(True):
-        for x_batch, y_batch in train_gen():
-            y_pred = pipeline(x_batch)
-            loss = mpjpe(y_pred, y_batch)
-
-            loss.backward()
+def train(cfg):
+    dataset = cfg.dataset(cfg)
+    pipeline = __import__('common.pipelines.' + cfg.pipeline, fromlist=[cfg.pipeline]).pipeline(dataset.num_joints, cfg)
+    
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from common.config import get_configs
-from common.Experiments import train
+from common.experiments.train import train
 
 cfgs = get_configs()
 
+train(cfgs[0])
