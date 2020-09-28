1e8ae9c9c5f2fad978099fa30747a554a7a74842
yongsyongs <ysn3830@gmail.com>
2020-09-28 19:02:32 +0900

    Create pipeline.py

---
+++ common/pipeline.py
@@ -0,0 +1,16 @@
+class Pipeline():
+    def __init__(self, modules=[], pred_func=None):
+        self.modules = modules
+
+        def pred(self, x):
+            for module in self.modules:
+                x = module(x)
+            return x
+
+        self.pred = pred if pred_func is None else pred_func
+
+    def set_pred(self, func):
+        self.pred = func
+
+    def __call__(self, x):
+        return self.pred(self, x)
